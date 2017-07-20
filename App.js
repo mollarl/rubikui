e8068468c5cbd83561524d8c18cd2cc0c59e1672
Luciano Mollar <lmollar@navent.com>
2017-07-20 12:55:15 -0300

    0.1

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = React.createClass({
+    getInitialState() {
+        return { num: Math.ceil(Math.random() * 6) };
+    },
+    getNumeroAleatorio() {
+        this.setState({num:Math.ceil(Math.random() * 6)});
+    },
+    render() {
+        return (
+            <div>
+                <p>{this.state.num}</p>
+                <button onClick={this.getNumeroAleatorio}>Lanzar</button>
+            </div>
+        );
+    }
+});
+
+export default App;
